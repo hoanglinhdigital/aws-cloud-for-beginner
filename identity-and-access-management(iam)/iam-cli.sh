f096a434a94b8bb3b4a026b356e2d687af4101f9
hoanglinhdigital <hoanglinhdigital@gmail.com>
2024-03-28 20:09:39 +0700

    remove secret info

--- identity-and-access-management(iam)/iam-cli.sh
+++ identity-and-access-management(iam)/iam-cli.sh
@@ -21,9 +21,9 @@ aws sts get-session-token --serial-number arn:aws:iam::123456789012:mfa/develope
 "C:\Users\<your user name>\.aws\credentials"
 #--------------------
 [udemy-mfa]
-aws_access_key_id = ASIAWIVVHVPNI6WO5AOW
-aws_secret_access_key = KKqlnJn6XT2RQWeNwuxQvhOWAjVbvSCbkRMEny9f
-aws_session_token = IQoJb3JpZ2luX2VjEK///////////wEaDmFwLXNvdXRoZWFzdC0xIkgwRgIhAO1Z8dMTyKyyDIcJBSSPBtnAUHpMUpQ3+mCeTyDpiK5PAiEAujswq262YxMSuHSmzU+1ZzoUqQfuZ19/Ut4NXqHgPfQq+AEIqP//////////ARAAGgw0MzA5NTA1NTg2ODIiDMkOIirsIpfYG0BbTirMAWDz7GCM632sxd6mGDY4ZfBmfBZuaVSm55P0ECkKt2qijTx8HsnijcOlwcnrXBjCdKwp126YaU6INtpecdCAYJGL4GH9enkwn+zQdT7eYN+io0Y8ciHMnA9MPPRAE8rJCAjA+FHVBZEY0zDqjGQQTr/BgWNiCR/e43orrikVXX+w3SfVRriTVsyma05A6o7bz1J0uz5mqG+AIHazGmr8Mek8fybV1t9YQDGmPxeYGfyausSOquitvJhJCocASWYUkSPCu12PMd9j4MdWYDCg0IqiBjqXAVLYQ7FVcMujq99V4azMjIDpIzLyasX9W/0J/VUIrxekFzdQE1SEfBNxInYgV8ER1TJ2WltKPmVH4rRb8n6Abtc4C40f1WDjDCcAqYdsrLQtk9hdKgSFD2pvnKK+vxvm8Wpexw6oVX4t77tncXAhqPAnQ8RnNcFSwtynszgIRZUqhGcapaEHQlrQ/4JKB0kXaxv8DlsiSF0=
+aws_access_key_id = <your-access-key-id>
+aws_secret_access_key = <your-secret-access-key>
+aws_session_token = <your-session-token>
 #-------------
 
 #Step 3: specify --profile when run command.
