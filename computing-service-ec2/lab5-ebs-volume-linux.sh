5b1437db15829f3dc6e8f580bec737c05016e942
hoanglinhdigital <hoanglinhdigital@gmail.com>
2025-04-27 21:11:48 +0900

    Re-record lab 5, ec2 extend linux volume

--- computing-service-ec2/lab5-ebs-volume-linux.sh
+++ computing-service-ec2/lab5-ebs-volume-linux.sh
@@ -4,14 +4,23 @@ lsblk
 #display all volume and free space
 df -h
 #Using fdisk to create partition.
-sudo fdisk /dev/xvdf
+sudo fdisk /dev/xvdb
+Nhan: 'n', default ... enter
+Nhan: 'w' để save lại.
+#Kiểm tra bằng lệnh sau, nếu thấy partiion tạo ra ngay dưới level disk là OK
+lsblk
 
 #format partition
-sudo mkfs -t xfs /dev/xvdf1
+sudo mkfs -t xfs /dev/xvdb1
+#Tạo folder để mount
+sudo mkdir /data
 #Mount
-sudo mount /dev/xvdf1 /data
+sudo mount /dev/xvdb1 /data
 
-#Make volume auto mount after restart
+#Test write data to /data
+sudo vi /data/test.txt
+
+#Make volume auto mount after restart EC2
 sudo blkid
 sudo lsblk -o +UUID
 
@@ -20,6 +29,14 @@ sudo vi /etc/fstab
 #Test auto mount
 sudo umount /data
 sudo mount -a
+df -h
 
-#Grow part after extend EBS volume on AWS Console.
-sudo growpart /dev/xvdf 1
+#Extend volume in AWS Console.
+#Login to Linux server, check disk and partition.
+lsblk
+#Grow Volume after extend EBS volume on AWS Console.
+sudo growpart /dev/xvdb 1
+#Grow part after extend volume
+sudo xfs_growfs -d /data
+#Kiem tra lai:
+df -h
