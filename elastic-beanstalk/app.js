7bb162d54bed78b785b51062b25a0f8b8d758292
hoanglinhdigital <hoanglinhdigital@gmail.com>
2025-06-08 22:47:37 +0900

    Re-record Elastic Beanstalk chapter

--- elastic-beanstalk/app.js
+++ elastic-beanstalk/app.js
@@ -2,17 +2,17 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') { 
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><h1 style="color:orange;">Welcome to Linh Website v0.1</h1></body></html>');
+        res.write('<html><body><h1 style="color:green;">Welcome to Linh Website v0.1</h1></body></html>');
         res.end();
     }
     else if (req.url == "/training") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><h1 style="color:green;">Welcome to Training page v0.1!</h1></body></html>');
+        res.write('<html><body><h1 style="color:blue;">Welcome to Training page v0.1!</h1></body></html>');
         res.end();
     }
     else if (req.url == "/admin") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><h1 style="color:black;">You need admin permission to access this page! -v0.1</h1></body></html>');
+        res.write('<html><body><h1 style="color:black;">You need admin permission to access this page! - v0.1</h1></body></html>');
         res.end();
     }
     else
